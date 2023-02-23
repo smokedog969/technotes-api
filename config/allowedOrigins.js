52286a8980763b19676162b0bc097aad509420fc
smokedog969 <steffen.schafrik@googlemail.com>
2023-02-23 16:52:55 +0100

    Allowed origins 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,10 @@
 const allowedOrigins = [
     'https://technotes-bgle.onrender.com',
+    'https://technotes-bgle.onrender.com/auth',
+    'https://technotes-bgle.onrender.com/login',
+    'https://technotes-bgle.onrender.com/logout',
+    'https://technotes-bgle.onrender.com/users',
+    'https://technotes-bgle.onrender.com/notes',
     'https://technotes-api.onrender.com',
     'https://technotes-api.onrender.com/auth',
     'https://technotes-api.onrender.com/login',
