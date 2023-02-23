cf448e24c4b548e9f7be97c9dd635c8dcc88cc25
smokedog969 <steffen.schafrik@googlemail.com>
2023-02-23 17:17:35 +0100

    neu

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ connectDB()
 
 app.use(logger)
 
-//app.use(cors(corsOptions))
-app.use(cors())
-
+app.use(cors(corsOptions))
+//app.use(cors())
+//app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 app.use(express.json())
 
 app.use(cookieParser())
