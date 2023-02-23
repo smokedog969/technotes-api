1c4e27dab986a234fbdf8cdeb9961e9f790d57a7
smokedog969 <steffen.schafrik@googlemail.com>
2023-02-23 07:01:31 +0100

    allowed origins 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'https://technotes-bgle.onrender.com',
-    'https://technotes-api.onrender.com'
+    'https://technotes-api.onrender.com',
+    'https://technotes-api.onrender.com/auth',
+    'https://technotes-api.onrender.com/login',
+    'https://technotes-api.onrender.com/logout',
 ]
 
 module.exports = allowedOrigins
