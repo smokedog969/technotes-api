8b8d7ee35ea36b0956287c119ab7934bdc19e793
smokedog969 <steffen.schafrik@googlemail.com>
2023-02-23 19:51:50 +0100

    3

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,6 @@
 const allowedOrigins = require('./allowedOrigins')
 
-const corsOptions = {
+/* const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
         //    if (allowedOrigins.indexOf(origin) !== -1) {
@@ -11,6 +11,25 @@ const corsOptions = {
     },
     credentials: true, // Required for cookies
     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+} */
+
+const corsOptions = {
+    origin: function(origin, callback){
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      }
+        /////
+        credentials: true, // Required for cookies
+        optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
+   
+
 
 module.exports = corsOptions
